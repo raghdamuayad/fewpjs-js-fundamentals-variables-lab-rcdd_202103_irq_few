bdcfe216180e0653f86102acfb0850154972ba17
Raghda  Muayad <therose000777@gmail.com>
2021-04-20 13:05:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+
+var myVariable = 'companyName';
+
+let companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+
+const companyName1 = 'mostProfitableNeighborhood';
+
+const company= ["Scuber"];
+
+it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});
