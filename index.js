292a0be52a630699c0a08a4d97873e8a09894db2
Raghda  Muayad <therose000777@gmail.com>
2021-04-22 21:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,16 @@
 var myVariable = 'companyName';
 
 let companyName = 'Scuber';
+<<<<<<< HEAD
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
 
+=======
+
+function calc("Chelsea","Susan Smith") {
+  return;
+}
+>>>>>>> 87b209d8c7ef7ab0ef9d21177210ba3108fa48b1
 
 const companyName1 = 'mostProfitableNeighborhood';
 
